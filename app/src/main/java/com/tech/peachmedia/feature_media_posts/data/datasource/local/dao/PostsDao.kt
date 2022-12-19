bbf762b81e48948ebcfeeb5756aa992253679e86
James Kirwa <jkirwa25@gmail.com>
2022-12-19 10:03:37 +0300

    - Fixed posts Dao quires

--- app/src/main/java/com/tech/peachmedia/feature_media_posts/data/datasource/local/dao/PostsDao.kt
+++ app/src/main/java/com/tech/peachmedia/feature_media_posts/data/datasource/local/dao/PostsDao.kt
@@ -7,9 +7,9 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PostsDao : CoroutineBaseDao<Post> {
-    @Query("SELECT post.documentId,post.authorID,post.mediaType,post.storageRef,post.caption,user.username,post.createdAt,post.createTime,post.updateTime FROM Post post LEFT JOIN User user ON user.id=post.authorID ")
+    @Query("SELECT post.documentId,post.authorID,post.mediaType,post.storageRef,post.caption,user.username,post.createdAt,post.createTime,post.updateTime FROM Post post JOIN User user ON user.userId=post.authorID ")
     fun getAllPosts(): Flow<List<PostView>>
 
-    @Query("SELECT  post.documentId,post.authorID,post.mediaType,post.storageRef,post.caption,user.username,post.createdAt,post.createTime,post.updateTime FROM Post post LEFT JOIN User user ON user.id=post.authorID WHERE mediaType=:mediaType")
+    @Query("SELECT  post.documentId,post.authorID,post.mediaType,post.storageRef,post.caption,user.username,post.createdAt,post.createTime,post.updateTime FROM Post post JOIN User user ON user.userId=post.authorID WHERE mediaType=:mediaType")
     fun filterPostByMediaType(mediaType: String): Flow<List<PostView>>
 }
